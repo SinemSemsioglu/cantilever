5d610c2af740ac21a75d0231515d0b0230dc3066
Sinem <sinem93@gmail.com>
2015-09-25 05:13:17 +0300

    nopee

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require("gulp"),
-    webserver = require("gulp-webserver"),
     browserSync = require("browser-sync").create(),
+    less = require("gulp-less"),
+    webserver = require("gulp-webserver"),
+    path = require("path"),
     url = require("url"),
-    eslint = require("gulp-eslint");
-
-var pathList = [];
+    eslint = require("gulp-eslint"),
+    concat = require("gulp-concat");
 
 gulp.task("default", ["watch"]);
+
+var pathList = ["src/bower_components/angular/angular.js", "src/bower_components/angular-route/angular-route.js",
+    "src/bower_components/angular-ui-router/release/angular-ui-router.min.js", "src/app.js",
+    "src/js/**"];
 
 gulp.task("browser-sync", function () {
 
     browserSync.init({
         "server": {
-            "baseDir": "src",
+            "baseDir": "dist",
             "index": "/index.html"
         }
     });
@@ -20,12 +25,12 @@ gulp.task("browser-sync", function () {
 });
 
 gulp.task("less", function () {
-    return gulp.src("src/style/onboarding.less")
+    return gulp.src("src/style/events.less")
         .pipe(less({
             "paths": [path.join(__dirname, "less", "includes")]
         }))
-        .pipe(concat("onboarding.css"))
-        .pipe(gulp.dest("style/"));
+        .pipe(concat("events.css"))
+        .pipe(gulp.dest("dist/style/"));
 });
 
 gulp.task("eslint", function () {
@@ -36,20 +41,40 @@ gulp.task("eslint", function () {
 
 });
 
-gulp.task("webserver", function() {
-    gulp.src("src")
+gulp.task("webserver", function () {
+    gulp.src("dist")
         .pipe(webserver({
             livereload: true,
-            directoryListing: true,
             open: true
         }));
 });
+
+gulp.task("scripts", function () {
+    return gulp.src(pathList)
+        .pipe(concat("events.js"))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("copyAssets", function () {
+    return gulp.src("src/assets/**")
+        .pipe(gulp.dest("dist/assets/"));
+});
+
+gulp.task("copyTemplates", function () {
+    return gulp.src("src/templates/**")
+        .pipe(gulp.dest("dist/templates/"));
+});
+
+gulp.task("copyIndex", function () {
+    return gulp.src("src/index.html")
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("build", ["less", "copyAssets", "copyTemplates", "scripts", "copyIndex"]);
 
 gulp.task("watch", function () {
     gulp.watch("/src/style/**", ["less"]);
     gulp.watch("/src/**").on("change", browserSync.reload);
 });
 
-gulp.task("dev-live", ["eslint", "browser-sync", "watch"]);
-
-gulp.task("build", ["webserver"]);
+gulp.task("dev-live", ["eslint", "build", "browser-sync", "watch"]);
