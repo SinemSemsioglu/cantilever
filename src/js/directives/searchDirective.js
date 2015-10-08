8d37053f129200d0eee2f0f20b8d1878f6a24324
Sinem <sinem93@gmail.com>
2015-10-08 11:49:47 +0300

    removing things

--- src/js/directives/searchDirective.js
+++
@@ -1,7 +0,0 @@
-app.directive("eventSearch", [function () {
-    return {
-        "restrict": "E",
-        "templateUrl": "templates/search.html",
-        "controller": "searchController"
-    }
-}]);
--- src/js/services/communicators/mainService.js
+++
@@ -1,9 +0,0 @@
-app.service("mainService", [function () {
-    var state = "date";
-    this.setEventState = function (newState) {
-        state = newState;
-    };
-    this.getEventState = function () {
-        return state;
-    };
-}]);
